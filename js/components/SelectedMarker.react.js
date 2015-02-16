95bc19c61fb1b6fbc6979e908e67bc1a182201ff
kenpeter <figo2476@hotmail.com>
2015-02-16 15:23:54 +1100

    init

---
+++ js/components/SelectedMarker.react.js
@@ -0,0 +1,31 @@
+var React = require('react');
+
+var SelectedMarker = React.createClass({
+
+  render: function() {
+    var selected = this.props.selected;
+    var currNodeKey = this.props.currNodeKey;
+
+		//test
+		/*
+		console.log('-s-');
+		console.log(selected);
+		console.log(currNodeKey);
+		console.log('-e-');
+		*/
+
+    if (typeof selected == 'undefined') {
+      return false;
+    } else {
+      if (selected == currNodeKey) {
+        return (
+          React.DOM.span({className: "selectedNode"}, ">>")
+        );
+      } else {
+        return false
+      }
+    }
+  }
+});
+
+module.exports = SelectedMarker;
